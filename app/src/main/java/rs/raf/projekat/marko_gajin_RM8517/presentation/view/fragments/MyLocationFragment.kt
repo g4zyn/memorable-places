3cd206a7b0219f47c6cda98eb748cf3630cd6b1c
Marko <markogajiin@gmail.com>
2020-06-19 00:13:15 +0200

    Google Maps API setup

--- app/src/main/java/rs/raf/projekat/marko_gajin_RM8517/presentation/view/fragments/MyLocationFragment.kt
+++ app/src/main/java/rs/raf/projekat/marko_gajin_RM8517/presentation/view/fragments/MyLocationFragment.kt
@@ -1,19 +1,60 @@
 package rs.raf.projekat.marko_gajin_RM8517.presentation.view.fragments
 
+import android.content.res.Resources
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MapStyleOptions
+import com.google.android.gms.maps.model.MarkerOptions
 import rs.raf.projekat.marko_gajin_RM85.R
+import timber.log.Timber
 
-class MyLocationFragment : Fragment(R.layout.fragment_my_location) {
+
+class MyLocationFragment : Fragment(R.layout.fragment_maps), OnMapReadyCallback {
+
+    private val markLocation = OnMapReadyCallback { googleMap ->
+        setMapStyle(googleMap)
+
+        val sydney = LatLng(-34.0, 151.0)
+        googleMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         init()
     }
 
-    private fun init() {}
+    private fun init() {
+        initMap()
+    }
 
+    private fun initMap() {
+        val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
+        mapFragment?.getMapAsync(this)
+    }
 
+    override fun onMapReady(googleMap: GoogleMap?) {
+        setMapStyle(googleMap)
+    }
 
+    private fun setMapStyle(googleMap: GoogleMap?) {
+        try {
+            val success = googleMap?.setMapStyle(
+                MapStyleOptions.loadRawResourceStyle(
+                    context, R.raw.mapstyle
+                )
+            )
+            if (!success!!) {
+                Timber.e("Style parsing failed.")
+            }
+        } catch (e: Resources.NotFoundException) {
+            Timber.e(e, "Can't find style. Error: ")
+        }
+    }
 }
