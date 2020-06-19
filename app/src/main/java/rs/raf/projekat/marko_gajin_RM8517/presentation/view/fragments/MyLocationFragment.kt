e237727449e03dddd4bfe5562a74c2324738b889
Marko <markogajiin@gmail.com>
2020-06-19 23:31:46 +0200

    home page bottom sheet implementation

--- app/src/main/java/rs/raf/projekat/marko_gajin_RM8517/presentation/view/fragments/MyLocationFragment.kt
+++ app/src/main/java/rs/raf/projekat/marko_gajin_RM8517/presentation/view/fragments/MyLocationFragment.kt
@@ -1,6 +1,7 @@
 package rs.raf.projekat.marko_gajin_RM8517.presentation.view.fragments
 
 import android.content.res.Resources
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
@@ -11,11 +12,14 @@ import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MapStyleOptions
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import kotlinx.android.synthetic.main.layout_home_sheet.*
+import kotlinx.android.synthetic.main.fragment_my_location.*
 import rs.raf.projekat.marko_gajin_RM85.R
 import timber.log.Timber
 
 
-class MyLocationFragment : Fragment(R.layout.fragment_maps) {
+class MyLocationFragment : Fragment(R.layout.fragment_my_location) {
 
     private val markLocation = OnMapReadyCallback { googleMap ->
         setMapStyle(googleMap)
@@ -31,12 +35,30 @@ class MyLocationFragment : Fragment(R.layout.fragment_maps) {
     }
 
     private fun init() {
+        initUi()
+        initListeners()
+    }
+
+    private fun initUi() {
         initMap()
+        initBottomSheet()
+        initListeners()
+    }
+
+    private fun initListeners() {
+        saveBtn.setOnClickListener {
+//            TODO  save place
+        }
     }
 
     private fun initMap() {
         val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
         mapFragment?.getMapAsync(markLocation)
+    }
+
+    private fun initBottomSheet() {
+        var bottomSheetBehavior = BottomSheetBehavior.from(bottomSheet)
+        bottomSheetBehavior.state = BottomSheetBehavior.STATE_COLLAPSED
     }
 
     private fun setMapStyle(googleMap: GoogleMap?) {
