da536b596ddc987fda145d383a2d8204f1ae0f4d
Marko <markogajiin@gmail.com>
2020-06-22 23:27:20 +0200

    Map initialization on PlacesFragment

--- app/src/main/java/rs/raf/projekat/marko_gajin_RM8517/presentation/view/fragments/PlacesFragment.kt
+++ app/src/main/java/rs/raf/projekat/marko_gajin_RM8517/presentation/view/fragments/PlacesFragment.kt
@@ -1,45 +1,30 @@
 package rs.raf.projekat.marko_gajin_RM8517.presentation.view.fragments
 
-import android.content.pm.PackageManager
 import android.content.res.Resources
-import android.location.Location
 import android.os.Bundle
 import android.view.View
-import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.add
-import com.google.android.gms.location.FusedLocationProviderClient
-import com.google.android.gms.location.LocationServices
-import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.*
+import com.google.android.gms.maps.model.MapStyleOptions
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import kotlinx.android.synthetic.main.layout_home_sheet.*
 import org.koin.androidx.viewmodel.ext.android.sharedViewModel
 import rs.raf.projekat.marko_gajin_RM85.R
-import rs.raf.projekat.marko_gajin_RM8517.data.models.Place
 import rs.raf.projekat.marko_gajin_RM8517.presentation.contracts.PlaceContract
 import rs.raf.projekat.marko_gajin_RM8517.presentation.viewmodels.PlaceViewModel
 import timber.log.Timber
 
-class MyLocationFragment : Fragment(R.layout.fragment_my_location) {
-
-    companion object {
-        private const val LOCATION_PERMISSION_REQ_CODE = 1
-    }
+class PlacesFragment : Fragment(R.layout.fragment_places) {
 
     private val placeViewModel: PlaceContract.ViewModel by sharedViewModel<PlaceViewModel>()
 
     private var mMap: GoogleMap? = null
-    private lateinit var fusedLocationClient: FusedLocationProviderClient
-    private lateinit var lastLocation: Location
 
     private val setMap = OnMapReadyCallback { googleMap ->
         mMap = googleMap
         setMapStyle()
-        getDeviceLocation()
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -57,24 +42,10 @@ class MyLocationFragment : Fragment(R.layout.fragment_my_location) {
         initBottomSheet()
     }
 
-    private fun initListeners() {
-        saveBtn.setOnClickListener {
-            val name = nameInput.text.toString()
-            val description = descInput.text.toString()
-            val newPlace = Place(
-                0,
-                name,
-                description,
-                lastLocation.latitude,
-                lastLocation.longitude
-            )
-            placeViewModel.savePlace(newPlace)
-        }
-    }
+    private fun initListeners() {}
 
     private fun initMap() {
-        fusedLocationClient = LocationServices.getFusedLocationProviderClient(requireActivity())
-        val mapFragment = childFragmentManager.findFragmentById(R.id.my_location_map) as SupportMapFragment?
+        val mapFragment = childFragmentManager.findFragmentById(R.id.places_map) as SupportMapFragment?
         mapFragment?.getMapAsync(setMap)
     }
 
@@ -97,30 +68,6 @@ class MyLocationFragment : Fragment(R.layout.fragment_my_location) {
         })
     }
 
-    private fun getDeviceLocation() {
-        if (ActivityCompat.checkSelfPermission(requireContext(),
-                android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(requireActivity(),
-                arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION), LOCATION_PERMISSION_REQ_CODE)
-            return
-        }
-        mMap?.isMyLocationEnabled = true
-
-        fusedLocationClient.lastLocation.addOnSuccessListener(requireActivity()) {location -> markLocation(location)}
-    }
-
-    private val markLocation = { location: Location ->
-        lastLocation = location
-        val currentLocation = LatLng(location.latitude, location.longitude)
-        placeMarkerOnMap(currentLocation)
-        mMap?.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLocation, 12f))
-    }
-
-    private fun placeMarkerOnMap(location: LatLng) {
-        val markerOptions = MarkerOptions().position(location)
-        mMap?.addMarker(markerOptions)
-    }
-
     private fun setMapStyle() {
         try {
             val success = mMap?.setMapStyle(
@@ -135,4 +82,5 @@ class MyLocationFragment : Fragment(R.layout.fragment_my_location) {
             Timber.e(e, "Can't find style. Error: ")
         }
     }
+
 }
--- app/src/main/java/rs/raf/projekat/marko_gajin_RM8517/presentation/view/viewpagers/NavigationAdapter.kt
+++ app/src/main/java/rs/raf/projekat/marko_gajin_RM8517/presentation/view/viewpagers/NavigationAdapter.kt
@@ -4,7 +4,7 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
 import rs.raf.projekat.marko_gajin_RM8517.presentation.view.fragments.MyLocationFragment
-import rs.raf.projekat.marko_gajin_RM8517.presentation.view.fragments.SavedLocationsFragment
+import rs.raf.projekat.marko_gajin_RM8517.presentation.view.fragments.PlacesFragment
 
 class NavigationAdapter(
     fragmentManager: FragmentManager
@@ -19,7 +19,7 @@ class NavigationAdapter(
     override fun getItem(position: Int): Fragment {
         return when (position) {
             FRAGMENT_MY_LOCATION -> MyLocationFragment()
-            else -> SavedLocationsFragment()
+            else -> PlacesFragment()
         }
     }
 
