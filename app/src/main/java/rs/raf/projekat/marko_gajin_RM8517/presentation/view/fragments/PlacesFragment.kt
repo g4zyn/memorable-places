925b99a11e217845d84f94a1f808bad4783116d3
Marko <markogajiin@gmail.com>
2020-06-25 02:09:07 +0200

    Marker on click listener implemented and code fix

--- app/src/main/java/rs/raf/projekat/marko_gajin_RM8517/presentation/view/fragments/PlacesFragment.kt
+++ app/src/main/java/rs/raf/projekat/marko_gajin_RM8517/presentation/view/fragments/PlacesFragment.kt
@@ -10,14 +10,15 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MapStyleOptions
+import com.google.android.gms.maps.model.Marker
+import com.google.android.gms.maps.model.MarkerOptions
 import com.google.android.material.bottomsheet.BottomSheetBehavior
-import kotlinx.android.synthetic.main.layout_home_sheet.*
-import kotlinx.android.synthetic.main.layout_home_sheet.bottomSheet
-import kotlinx.android.synthetic.main.layout_home_sheet.headerBtn
 import kotlinx.android.synthetic.main.layout_places_sheet.*
 import org.koin.androidx.viewmodel.ext.android.sharedViewModel
 import rs.raf.projekat.marko_gajin_RM85.R
+import rs.raf.projekat.marko_gajin_RM8517.data.models.Place
 import rs.raf.projekat.marko_gajin_RM8517.presentation.contracts.PlaceContract
 import rs.raf.projekat.marko_gajin_RM8517.presentation.view.recycler.adapters.PlaceAdapter
 import rs.raf.projekat.marko_gajin_RM8517.presentation.view.states.PlacesState
@@ -28,14 +29,10 @@ class PlacesFragment : Fragment(R.layout.fragment_places) {
 
     private val placeViewModel: PlaceContract.ViewModel by sharedViewModel<PlaceViewModel>()
 
+    private lateinit var bottomSheetBehavior: BottomSheetBehavior<View>
     private lateinit var adapter: PlaceAdapter
 
     private var mMap: GoogleMap? = null
-
-    private val setMap = OnMapReadyCallback { googleMap ->
-        mMap = googleMap
-        setMapStyle()
-    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -54,21 +51,26 @@ class PlacesFragment : Fragment(R.layout.fragment_places) {
         initListeners()
     }
 
+    private fun initObservers() {
+        placeViewModel.placesState.observe(viewLifecycleOwner, Observer {
+            Timber.e("Place - $it")
+            renderState(it)
+        })
+        placeViewModel.getPlaces()
+    }
+
     private fun initMap() {
         val mapFragment = childFragmentManager.findFragmentById(R.id.places_map) as SupportMapFragment?
         mapFragment?.getMapAsync(setMap)
     }
 
-    private fun initBottomSheet() {
-        val bottomSheetBehavior = BottomSheetBehavior.from(bottomSheet)
+    private val setMap = OnMapReadyCallback { googleMap ->
+        mMap = googleMap
+        setMapStyle()
+    }
 
-        bottomSheet.setOnClickListener {
-            if (bottomSheetBehavior.state != BottomSheetBehavior.STATE_EXPANDED) {
-                bottomSheetBehavior.state = BottomSheetBehavior.STATE_EXPANDED
-            } else {
-                bottomSheetBehavior.state = BottomSheetBehavior.STATE_COLLAPSED
-            }
-        }
+    private fun initBottomSheet() {
+        bottomSheetBehavior = BottomSheetBehavior.from(bottomSheet)
 
         bottomSheetBehavior.addBottomSheetCallback(object : BottomSheetBehavior.BottomSheetCallback() {
             override fun onSlide(bottomSheet: View, slideOffset: Float) {
@@ -84,14 +86,28 @@ class PlacesFragment : Fragment(R.layout.fragment_places) {
         placesRv.adapter = adapter
     }
 
-    private fun initListeners() {}
+    private fun initListeners() {
+        mMap?.setOnMarkerClickListener { showLocation(it) }
+        bottomSheet.setOnClickListener { collapseBottomSheet(it) }
+    }
 
-    private fun initObservers() {
-        placeViewModel.placesState.observe(viewLifecycleOwner, Observer {
-            Timber.e("Place - $it")
-            renderState(it)
-        })
-        placeViewModel.getPlaces()
+    private val showLocation = { marker: Marker ->
+        if (marker.isInfoWindowShown) {
+            marker.hideInfoWindow()
+//            TODO hide location info sheet
+        } else {
+            marker.showInfoWindow()
+//            TODO show location info sheet
+        }
+        true
+    }
+
+    private val collapseBottomSheet = { _: View ->
+        if (bottomSheetBehavior.state != BottomSheetBehavior.STATE_EXPANDED) {
+            bottomSheetBehavior.state = BottomSheetBehavior.STATE_EXPANDED
+        } else {
+            bottomSheetBehavior.state = BottomSheetBehavior.STATE_COLLAPSED
+        }
     }
 
     private fun renderState(state: PlacesState) {
@@ -99,6 +115,7 @@ class PlacesFragment : Fragment(R.layout.fragment_places) {
             is PlacesState.Success -> {
                 showLoadingState(false)
                 adapter.submitList(state.places)
+                state.places.forEach { markLocation(it) }
             }
             is PlacesState.Error -> {
                 showLoadingState(false)
@@ -108,6 +125,12 @@ class PlacesFragment : Fragment(R.layout.fragment_places) {
                 showLoadingState(true)
             }
         }
+    }
+
+    private val markLocation = { place: Place ->
+        val location = LatLng(place.latitude, place.longitude)
+        val markerOptions = MarkerOptions().position(location).title(place.name)
+        mMap?.addMarker(markerOptions)
     }
 
     private fun showLoadingState(loading: Boolean) {
